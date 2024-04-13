88d2a6ada72dcd557752cbb5cbda9cfb9d58eece
bawan <brwa.test1@gmail.com>
2024-04-13 23:09:30 +0300

    zad krdni ClipRRect (borderRadius) w AspectRatio(drezyw pani ada child babe away la gawra bwna tek bchet .bo file stock bw

--- lib/stack.dart
+++ lib/stack.dart
@@ -15,25 +15,51 @@ class _MyWidgetState extends State<MyWidget> {
         appBar: AppBar(
           title: Text('My Widget'),
         ),
-        body: Stack(
-          //clipBehavior: It determines whether the content will be clipped or not.
-          //overflow: It controls the children widgets, whether visible or clipped, when it's content overflowing outside the stack.
-          clipBehavior: Clip.none, // Allow overflow to be visible
-          children: <Widget>[
-            Container(
-              width: 100,
-              height: 100,
-              color: Colors.black12,
-            ),
-            Positioned(
-              bottom: 0,
-              right: 0,
-              child: Container(
-                width: 50,
-                height: 50,
-                color: Color.fromARGB(115, 218, 28, 28),
+        //agar AspectRatio 1 raqam hight 2 with agar bcheke gawray bka awa awa drezhy pany nagoret
+        //agar eshy nakrd awa ailgnment lagala bakar bena 
+        body: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 3/2,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(7),
+                    child: Stack(
+                      //clipBehavior: It determines whether the content will be clipped or not.
+                      //overflow: It controls the children widgets, whether visible or clipped, when it's content overflowing outside the stack.
+                      clipBehavior: Clip.none, // Allow overflow to be visible
+                      children: <Widget>[
+                        Container(
+                          
+                          color: Colors.black12,
+                        ),
+            
+                        AspectRatio(
+                          aspectRatio: 5/2,
+                          child: Center(
+                            child: Container(
+                              width: 100,
+                              height: 100,
+                              color: Color.fromARGB(115, 218, 28, 28),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
+            ClipOval(
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.blue,
+              ),
+            )
           ],
         ),
       ),
